maniakalochka/python-section

[feat] [project] initial commit

src/wallets/currency.py
@@ -1,0 +1,26 @@
+import enum
+from dataclasses import dataclass
+
+__all__ = ("AvailableCurrency", "Currency", "rub", "usd")
+
+
+class AvailableCurrency(enum.Enum):
+    RUB = enum.auto()
+    USD = enum.auto()
+
+
+@dataclass(slots=True, frozen=True, eq=True, repr=True)
+class Currency:
+    code = AvailableCurrency
+
+
+class RUB(Currency):
+    currency: Currency = AvailableCurrency.RUB
+
+
+class USD(Currency):
+    currency: Currency = AvailableCurrency.USD
+
+
+rub = RUB()
+usd = USD()

Esmien/python-section-em

src/wallets/currency.py
@@ -1,0 +1,28 @@
+import enum
+from dataclasses import dataclass
+
+__all__ = ("AvailableCurrency", "Currency", "rub", "usd")
+
+
+class AvailableCurrency(enum.Enum):
+    RUB = enum.auto()
+    USD = enum.auto()
+
+
+@dataclass(slots=True, frozen=True, eq=True, repr=True)
+class Currency:
+    code: AvailableCurrency
+
+
+@dataclass(slots=True, frozen=True, eq=True, repr=True)
+class RUB(Currency):
+    code: Currency = AvailableCurrency.RUB
+
+
+@dataclass(slots=True, frozen=True, eq=True, repr=True)
+class USD(Currency):
+    code: Currency = AvailableCurrency.USD
+
+
+rub = RUB()
+usd = USD()