341cea4debcc58320d682b2b1daa26d607779323
Владислав Сороковых <black.walf16@gmail.com>
2026-03-04 18:34:35 +0300

    Реализован итератор и генератор для вычисления чисел фибоначчи

--- src/iterators/utils.py
+++ src/iterators/utils.py
@@ -32,5 +32,41 @@ class RetrieveRemoteData:
     pass
 
 
+# class Fibo:
+#     def __init__(self, n: int) -> None:
+#         self.n = n
+#
+#     def __iter__(self):
+#         a, b = 0, 1
+#         for _ in range(self.n):
+#             yield a
+#             a, b = b, a + b
+
 class Fibo:
-    pass
+    def __init__(self, n: int) -> None:
+        """ Инициализация итератора """
+
+        if n < 0:
+            raise ValueError("Требуется положительное число")
+        self.n = n
+        self.pos = 0
+        self.a, self.b = 0, 1
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """ Возвращает следующий элемент последовательности """
+
+        # Проверка на конец последовательности
+        if self.pos >= self.n:
+            raise StopIteration
+
+        current = self.a
+
+        # Вычисление следующих значений и переход к следующему элементу
+        self.a, self.b = self.b, self.a + self.b
+        self.pos += 1
+
+        return current
